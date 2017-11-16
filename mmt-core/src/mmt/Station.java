c32014a8814df0d6e5fc1babedea477b30bf0884
João Daniel Silva <joaodaniel1998@gmail.com>
2017-11-16 00:26:08 +0000

    station com dois metodos, rever getLocalTime

--- mmt-core/src/mmt/Station.java
+++ mmt-core/src/mmt/Station.java
@@ -1,5 +1,24 @@
 package mmt;
+import java.time.LocalTime;
 
 public class Station {
-
+    private string _name;
+    //uma estacao tem um conjunto de partidas a uma dada hora
+    //cada partida corresponde a parte de um servico
+    private TreeMap<int, LocalTime> _partidas;
+    
+    Station(String name, LocalTime time) {
+        _name = name;
+        TreeMap<int, LocalTime> _partidas = new TreeMap<int, _partidas>;
+    }
+    
+    //rever este metodo na class Service
+    public LocalTime getLocalTimeInService(int serviceID) {
+        return _partidas.get(serviceID);
+    }
+    
+    //em principio nao vai ser usado?
+    public void setLocalTimeInService(int serviceID,LocalTime horasPartida) {
+        _partidas.put(serviceID, horasPartida);
+    }
 }
