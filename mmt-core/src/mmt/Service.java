eee0827397ba78b3441de0ca5c2e53d6cd56b854
João Daniel Silva <joaodaniel1998@gmail.com>
2017-11-16 16:51:37 +0000

    Station - partidas em ArrayList

--- mmt-core/src/mmt/Service.java
+++ mmt-core/src/mmt/Service.java
@@ -10,9 +10,10 @@ public class Service {
     private int _id;
     private double _price;
     private TreeMap<String, Station> _stations;
-	
+    //fg
+
     public Service(int id, double price, List<Station> ls) {
-        _stations = new TreeMap<String, Station>(new COMPARE_STATION_BY_HOUR() );
+        _stations = new TreeMap<String, Station>;
         _id = id;
         _price = price;
         for(Station s: ls) {
@@ -20,39 +21,40 @@ public class Service {
         }
 
     }
-    
+
     public Map<String,Station> getStations() {
         return _stations;
     }
-    
+
     //GETTERS
 	public int getServiceID() {return _id;}
-	
+
 	public double getServicePrice() {return _price;}
-	
+
     public Station getServiceDeparture() {
         return _stations.get(_stations.firstKey());
-		
+
     }
 
     public Station getServiceArrival(){
         return _stations.get(_stations.lastKey());
     }
-	
-	
+
+
 	//COMPARATORS
     public final  Comparator<Service> ARRIVAL_TIME_CMP = new ArrivalTimeComparator();
-    
+
     public final Comparator<Service> DEPARTURE_TIME_CMP = new DepartureTimeComparator();
-    
+
+    /*
     public final Comparator<Station> COMPARE_STATION_BY_HOUR = new CompareStationByHour();
-    
+
     public class CompareStationByHour implements Comparator<Station> {
         public int compare(Station station1, Station station2, int ServiceID) {
             return station1.getLocalTimeInService(ServiceID).compareTo(station2.getLocalTimeInService(ServiceID));
         }
-    }
-    
+    }*/
+
     private class ArrivalTimeComparator implements Comparator<Service> {
         public int compare(Service service1, Service service2) {
             return service1.getServiceArrival() - service2.getServiceArrival();
@@ -61,8 +63,8 @@ public class Service {
 
     private class DepartureTimeComparator implements Comparator<Service> {
         public int compare(Service service1, Service service2) {
-            return service1.getServiceArrivaDeparture() - service2.getServiceDeparture();
+            return service1.getServiceArrivaDeparture().compareTo(service2.getServiceDeparture());
         }
     }
-    
+
 }
--- mmt-core/src/mmt/Station.java
+++ mmt-core/src/mmt/Station.java
@@ -1,26 +1,27 @@
 package mmt;
 import java.time.LocalTime;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.ArrayList;
 
 public class Station {
     private String _name;
-    //uma estacao tem um conjunto de partidas a uma dada hora
-    //cada partida corresponde a parte de um servico
-    private TreeMap<Integer, LocalTime> _partidas;
-    
+    //aray de horas em que o index de cada posicao corresponde a um servico
+    private ArrayList<LocalTime> _partidas;
+
     Station(String name, LocalTime time) {
         _name = name;
-        _partidas = new TreeMap<Integer, LocalTime>();
+        _partidas = new ArrayList<Integer, LocalTime>();
     }
-    
+
     //rever este metodo na class Service
     public LocalTime getLocalTimeInService(int serviceID) {
         return _partidas.get(serviceID);
     }
-    
+
     //em principio nao vai ser usado?
     public void setLocalTimeInService(int serviceID,LocalTime horasPartida) {
-        _partidas.put(serviceID, horasPartida);
+        _partidas.set(serviceID, horasPartida);
     }
+
+    //GETTERS
+    public String getName() {return _name;}
 }
