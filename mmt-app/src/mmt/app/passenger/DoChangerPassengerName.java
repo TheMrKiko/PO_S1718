dcc4cd25cc482cfb43f1cdc566a8a7052b0bc7b7
João Daniel Silva <joaodaniel1998@gmail.com>
2017-11-17 14:32:44 +0000

    AAAAAA

--- mmt-app/src/mmt/app/passenger/DoChangerPassengerName.java
+++ mmt-app/src/mmt/app/passenger/DoChangerPassengerName.java
@@ -19,18 +19,35 @@ import pt.tecnico.po.ui.Input;
 public class DoChangerPassengerName extends Command<TicketOffice> {
 
   //FIXME define input fields
-
+  Input<String> _newName;
+  Input<Integer> _id;
   /**
    * @param receiver
    */
   public DoChangerPassengerName(TicketOffice receiver) {
     super(Label.CHANGE_PASSENGER_NAME, receiver);
     //FIXME initilize input fields
+    _id = _form.addIntegerInput(Message.requestPassengerId());
+    _newName = _form.addStringInput(Message.requestPassengerName());
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() throws DialogException {
     //FIXME implement command
+    _form.parse();
+    try {
+        Passenger p = _receiver.getPassengerById(_id.value());
+        p.changePassengerName(_newName.value());
+    }
+    catch (NonUniquePassengerNameException e) {
+    	throw new DuplicatePassengerNameException(_newName.value());
+    }
+    catch (NoSuchPassengerIdException e) {
+        throw new NoSuchPassengerException(_newName.value());
+    }
+    catch (InvalidPassengerNameException e) {
+        throw new BadPassengerNameException(_newName.value());
+    }
   }
 }
--- mmt-core/src/mmt/Service.java
+++ mmt-core/src/mmt/Service.java
@@ -7,8 +7,10 @@ import java.util.LinkedHashMap;
 import java.util.ArrayList;
 
 public class Service {
-    /*private int _id;
+    private int _id;
     private double _price;
+    private String _firstStation;
+    private String _lastStation;
     //private HashMap<String, Station> _stations;
     private LinkedHashMap<String,LocalTime> _stations;
     public Service(int id, double price) {
@@ -30,14 +32,57 @@ public class Service {
 
 	public double getServicePrice() {return _price;}
 
-    public Station getServiceDeparture() {
-        return _stations.get(_stations.firstKey());
+
+    //SERVICE DEPARTURE/PARTIDA
+    public String getServiceDepartureStation() {
+        if(_firstStation == NULL) {
+            _firstStation = getServiceFirstStation(); //guarda o valor para nao ter de iterar de novo depois
+        }
+        return _firstStation;
     }
 
-    public Station getServiceArrival(){
-        return _stations.get(_stations.lastKey());
+    public LocalTime getServiceDepartureTime() {
+        if(_firstStation == NULL) {
+            _firstStation = getServiceFirstStation(); //guarda o valor para nao ter de iterar de novo depois
+        }
+        return _stations.get(_firstStation);
     }
 
+    public String getServiceFirstStation() { //metodo para nao haver repeticao de codigo na procura da primeira estacao
+        return  _stations.keySet().iterator().next();
+    }
+
+    //SERVICE ARRIVAL//TERMINO
+    public String getServiceArrivalStation() {
+        if(_lastStation == NULL) {
+            _lastStation = getServiceLastStation(); //guarda o valor para nao ter de iterar de novo depois
+        }
+        return _lastStation;
+    }
+
+    public LocalTime getServiceArrivalTime() {
+        if(_lastStation == NULL) {
+            _lastStation = getServiceLastStation(); //guarda o valor para nao ter de iterar de novo depois
+        }
+        return _stations.get(_lastStation);
+
+    }
+
+    public String getServiceLastStation() { //metodo para nao haver repeticao de codigo na procura da primeira estacao
+        for (String stationName : _stations.keySet()){
+            //loop
+        }
+        return stationName;
+    }
+
+    public String getServiceLastStation() {
+        final Iterator itr = _stations.keySet().iterator();
+        String stationName;
+        while(itr.hasNext()) {
+            stationName = itr.next();
+        }
+        return stationName;
+    }
 	//COMPARATORS
     public final  Comparator<Service> ARRIVAL_TIME_CMP = new ArrivalTimeComparator();
 
@@ -47,21 +92,8 @@ public class Service {
     //Compara o tempo de chegada a estacao de termino, independepndentemente da estacao de cada servico
     private class ArrivalTimeComparator implements Comparator<Service> {
         public int compare(Service service1, Service service2) {
-            String stationArrival1 = service1.getServiceArrival(); //estacao de onde termina o servico
-            String stationArrival2 = service2.getServiceArrival();
-            LocalTime arrivalTime1 = _stations.get(stationArrival1); //a que horas chega a estacao de termino
-            LocalTime arrivalTime2 = _stations.get(stationArrival2);
-            return arrivalTime1.compareTo(arrivalTime2);
-        }
-    }
-
-    //Compara o tempo de chegada a estacao de termino,sendo a mesma estacao para os dois servicos
-    private class SameStationArrivalTimeComparator implements Comparator<Service> {
-        public int compare(Service service1, Service service2) {
-            String stationArrival1 = service1.getServiceArrival(); //estacao de onde termina o servico
-            String stationArrival2 = stationArrival1;
-            LocalTime arrivalTime1 = _stations.get(stationArrival1); //a que horas chega a estacao de termino
-            LocalTime arrivalTime2 = _stations.get(stationArrival2);
+            LocalTime arrivalTime1 = service1.getServiceArrivalTime();//a que horas chega a estacao de termino
+            LocalTime arrivalTime2 = service2.getServiceArrivalTime();
             return arrivalTime1.compareTo(arrivalTime2);
         }
     }
@@ -69,27 +101,39 @@ public class Service {
     //Compara o tempo de partida da estacao de inicio, independepndentemente da estacao de cada servico
     private class DepartureTimeComparator implements Comparator<Service> {
         public int compare(Service service1, Service service2) {
-            String stationDeparture1 = service1.getServiceDeparture();
-            String stationDeparture2 = service2.getServiceDeparture();
-            LocalTime departureTime1 = _stations.get(stationDeparture1); //a que horas parte da estacao de inicio
-            LocalTime departureTime2 = _stations.get(stationDeparture2);
+            LocalTime departureTime1 = service1.getServiceDepartureTime();//a que horas parte da estacao de inicio
+            LocalTime departureTime2 = service2.getServiceDepartureTime();
             return departureTime1.compareTo(departureTime2);
+        }
+    }
+
+    /*** COMPARATORS JA DESNECESSARIOS?*/
+    //Compara o tempo de chegada a estacao de termino,sendo a mesma estacao para os dois servicos
+    /*private class SameStationArrivalTimeComparator implements Comparator<Service> {
+        public int compare(Service service1, Service service2) {
+            String stationArrival1 = service1.getServiceArrival();
+            String stationArrival2 = stationArrival1;
+            LocalTime arrivalTime1 = _stations.get(stationArrival1);
+            LocalTime arrivalTime2 = _stations.get(stationArrival2);
+            return arrivalTime1.compareTo(arrivalTime2);
         }
     }
 
     //Compara o tempo de chegada a estacao de termino,sendo a mesma estacao para os dois servicos
-    private class SameStationDepartureTimeComparator implements Comparator<Service> {
+    /*private class SameStationDepartureTimeComparator implements Comparator<Service> {
         public int compare(Service service1, Service service2) {
             String stationDeparture1 = service1.getServiceDeparture();
             String stationDeparture2 = stationDeparture1;
-            LocalTime departureTime1 = _stations.get(stationDeparture1); //a que horas parte da estacao de inicio
+            LocalTime departureTime1 = _stations.get(stationDeparture1);
             LocalTime departureTime2 = _stations.get(stationDeparture2);
             return departureTime1.compareTo(departureTime2);
         }
     }
+    ****/
+
 
     //SE HOVER CLASS ESTACAO NAO LEIAS ENTAO ISTO FRANCISCO
-    Construtor
+    /*Construtor
     public Service(int id, double price, ArrayList<Station> ls) {
         _stations = new HashMap<String, Station>;
         _id = id;
@@ -97,6 +141,13 @@ public class Service {
         for(Station s: ls) {
             _stations.put(s.getName(), s);
         }
+    }
+    public Station getServiceDeparture() {
+        return _stations.get(_stations.firstKey());
+    }
+
+    public Station getServiceArrival(){
+        return _stations.get(_stations.lastKey());
     }
 
     //Comparators
