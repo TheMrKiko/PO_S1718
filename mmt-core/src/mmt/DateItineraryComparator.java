5a28caf846152579d4e908ba4e38956ccf7975f1
João Daniel Silva <joaodaniel1998@gmail.com>
2017-12-07 19:52:18 +0000

    DateItineraryComparator

---
+++ mmt-core/src/mmt/DateItineraryComparator.java
@@ -0,0 +1,21 @@
+package mmt;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Comparator;
+
+public class DateItineraryComparator implements Comparator<Itinerary>, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8134199415336132244L;
+
+	@Override
+	public int compare(Itinerary itinerary1, Itinerary itinerary2) {
+		LocalDate date1 = itinerary1.getDate();
+		LocalDate date2 = itinerary2.getDate();
+		return date1.compareTo(date2);
+	}
+
+}
