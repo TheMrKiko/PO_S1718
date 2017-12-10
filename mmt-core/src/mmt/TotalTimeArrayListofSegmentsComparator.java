ee488fdb8b09960508ba04072ae4a909b537e045
João Daniel Silva <joaodaniel1998@gmail.com>
2017-12-10 00:43:23 +0000

    total time arraylist of segments comparator

---
+++ mmt-core/src/mmt/TotalTimeArrayListofSegmentsComparator.java
@@ -0,0 +1,28 @@
+package mmt;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class TotalTimeArrayListofSegmentsComparator implements Serializable, Comparator<ArrayList<Segment>> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1753162820247144944L;
+
+	@Override
+	public int compare(ArrayList<Segment> al1, ArrayList<Segment> al2) {
+		Duration duration1 = Duration.ZERO;
+		Duration duration2 = Duration.ZERO;
+		for (Segment s : al1) {
+			duration1 = duration1.plus(s.getDuration());
+		}
+		for (Segment s : al1) {
+			duration2 = duration2.plus(s.getDuration());
+		}
+		return duration1.compareTo(duration2);
+	}
+
+}
